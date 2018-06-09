627fd9137176243d1ee090f2b3a9cfcdec9639a1
Stephen Phillips <chengdu.scp@gmail.com>
2018-06-09 16:38:43 -0400

    updated download_files to have multiple runs allowed

--- launch/download_files.py
+++ launch/download_files.py
@@ -6,14 +6,13 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_log(yaml_name):
+def get_logs(yaml_name):
   # raw = subprocess.check_output(["kubectl", "get", "pods", "--show-all"])
   raw = subprocess.check_output(["kubectl", "get", "pods"])
   decoded = [ z.split() for z in raw.decode("utf-8").split("\n") ]
   log_names = [ w[0] for w in decoded[:-1] if os.path.splitext(yaml_name)[0] in w[0] ]
   logs_bytes = [ subprocess.check_output(["kubectl", "logs", log_name]) for log_name in log_names ]
-  l = max(range(len(logs_bytes)),key=lambda i: len(logs_bytes[i]))
-  return logs_bytes[l].decode("utf-8").split("\n")
+  return [ lb.decode("utf-8").split("\n") for lb in logs_bytes ]
 
 def get_loss_values(log):
   loss = []
@@ -28,21 +27,31 @@ losses = []
 for n in range(1,len(sys.argv)):
   yaml_name = sys.argv[n]
   name = os.path.splitext(yaml_name)[0]
-  log = get_log(yaml_name)
-  loss = np.array(get_loss_values(log))
-  losses.append((loss,name))
+  logs = get_logs(yaml_name)
+  if len(logs) > 1:
+    for l in range(len(logs)):
+      loss = np.array(get_loss_values(logs[l]))
+      losses.append((loss,"{}-{}".format(name,l),name))
+  else:
+    loss = np.array(get_loss_values(logs[0]))
+    losses.append((loss,name,name))
 
-print([ len(loss) for loss, name in losses])
+print([ len(loss) for loss, name, _ in losses])
 path="/home/stephen/cycleconsistencynet/save/save"
-for loss, name in losses:
+for loss, name, pathname in losses:
   dirpath = "../logs/save-{}".format(name)
   if not os.path.exists(dirpath):
-    subprocess.call(["scp", "-r", "stephen@kostas-ap.grasp.upenn.edu:{}-{}".format(path,name), "../logs"])
+    subprocess.call([
+      "scp",
+      "-r",
+      "stephen@kostas-ap.grasp.upenn.edu:{}-{}".format(path,pathname),
+      dirpath
+    ])
     np.save(os.path.join(dirpath, 'loss.npy'), loss)
 
 for i in range(len(losses)):
   plt.plot(losses[i][0], label=losses[i][1])
 plt.legend()
 plt.show()
-print([ loss[-1] for loss, name in losses ])
+print([ loss[-1] for loss, name, _ in losses ])
 
