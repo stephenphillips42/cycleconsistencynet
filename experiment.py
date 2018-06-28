03ffc240a7749e02af67c7989054da13fb8fd887
Stephen Phillips <chengdu.scp@gmail.com>
2018-06-28 14:53:58 -0400

    Fixed up experiment to give pretty plots'

--- experiment.py
+++ experiment.py
@@ -28,9 +28,6 @@ def get_sorted(sample):
   slabels = labels[sorted_idxs]
   return slabels, sorted_idxs
 
-def get_sims():
-  pass
-
 def plot_baseline(opts, network, index):
   sample = npload(os.path.join(opts.data_dir, 'np_test'), index)
   slabels, sorted_idxs = get_sorted(sample)
@@ -55,83 +52,51 @@ def plot_baseline(opts, network, index):
 def plot_index(opts, network, index):
   sample = npload(os.path.join(opts.data_dir, 'np_test'), index)
   output = network.apply_np(sample)
-  pass
-
-def experiment(opts, network, index):
-  # Load sample
-  sample = npload(os.path.join(opts.data_dir, 'np_test'), index)
-  output = network.apply_np(sample)
-  # Sort by ground truth for better visualization
-  slabels, soutput = get_sorted(sample)
-  # Plot setup
-  t = list(range(shape[0]))
-  xx0, yy0 = np.meshgrid(t, t)
-  t = list(range(shape[1]))
-  xx1, yy1 = np.meshgrid(t, t)
+  slabels, sorted_idxs = get_sorted(sample)
+  soutput = output[sorted_idxs]
+  srand = myutils.dim_normalize(sample['InitEmbeddings'][sorted_idxs])
   lsim = np.abs(np.dot(slabels, slabels.T))
   osim = np.abs(np.dot(soutput, soutput.T))
-  rsim = np.abs(np.dot(rand, rand.T))
-  if opts.debug_plot == 'plot':
-    print('Sorted labels')
-    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3)
-    im0 = ax0.imshow(slabels)
-    im1 = ax1.imshow(soutput)
-    im2 = ax2.imshow(rand)
-    fig.colorbar(im1, ax=ax1)
-    fig.colorbar(im2, ax=ax2)
-    plt.show()
-    print('Sorted similarites')
-    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3)
-    im0 = ax0.imshow(lsim)
-    im1 = ax1.imshow(osim)
-    im2 = ax2.imshow(rsim)
-    fig.colorbar(im1, ax=ax1)
-    fig.colorbar(im2, ax=ax2)
-    plt.show()
-
-  # Printing out parts
-  npts = len(np.unique(idxs))
-  mean_sims = np.zeros((npts,npts))
-
+  rsim = np.abs(np.dot(srand, srand.T))
+  fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
+  im0 = ax0.imshow(soutput)
+  im1 = ax1.imshow(osim)
+  fig.colorbar(im0, ax=ax0)
+  fig.colorbar(im1, ax=ax1)
+  plt.show()
   diag = np.reshape(osim[lsim==1],-1)
   off_diag = np.reshape(osim[lsim==0],-1)
   baseline_diag = np.reshape(rsim[lsim==1],-1)
   baseline_off_diag = np.reshape(rsim[lsim==0],-1)
-  stats = (np.mean(diag), np.std(diag), \
-           np.mean(off_diag), np.std(off_diag), \
-           np.mean(baseline_diag), np.std(baseline_diag), \
-           np.mean(baseline_off_diag), np.std(baseline_off_diag))
-  if opts.verbose:
-    print("Diag: {:.2e} +/- {:.2e}, Off Diag: {:.2e} +/- {:.2e}, " \
-          "Baseline Diag: {:.2e} +/- {:.2e}, " \
-          "Baseline Off Diag: {:.2e} +/- {:.2e}".format(*stats))
+  fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
+  ax0.hist([ diag, baseline_diag ], bins=20, normed=1,
+           label=[ 'diag', 'baseline_diag' ])
+  ax0.legend()
+  ax0.set_title('Diagonal Similarity Rate')
+  ax1.hist([ off_diag, baseline_off_diag ], bins=20, normed=1,
+           label=[ 'off_diag', 'baseline_off_diag' ])
+  ax1.set_title('Off Diagonal Similarity Rate')
+  ax1.legend()
+  plt.show()
 
-  if opts.debug_plot == 'plot':
-    i, j, k = 0, 1, 2
-    l = np.concatenate([
-          output[idxs == i, :],
-          output[idxs == j, :],
-          output[idxs == k, :]
-        ])
-    print('Similarities')
-    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3)
-    im0 = ax0.imshow(np.dot(l, l.T))
-    im1 = ax1.imshow(l)
-    im2 = ax2.imshow(mean_sims)
-    fig.colorbar(im0, ax=ax0)
-    plt.show()
-    print('Diagonal and off diagonal histograms')
-    fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
-    ax0.hist([ diag, baseline_diag ], bins=20, normed=1, label=[ 'diag', 'baseline_diag' ])
-    ax0.legend()
-    ax0.set_title('Diagonal Similarity Rate')
-    ax1.hist([ off_diag, baseline_off_diag ], bins=20, normed=1, label=[ 'diag', 'baseline_diag' ])
-    ax1.set_title('Off Diagonal Similarity Rate')
-    ax1.legend()
-    plt.show()
+def get_stats(opts, network, index):
+  sample = npload(os.path.join(opts.data_dir, 'np_test'), index)
+  output = network.apply_np(sample)
+  slabels, sorted_idxs = get_sorted(sample)
+  soutput = output[sorted_idxs]
+  srand = myutils.dim_normalize(sample['InitEmbeddings'][sorted_idxs])
+  lsim = np.abs(np.dot(slabels, slabels.T))
+  osim = np.abs(np.dot(soutput, soutput.T))
+  rsim = np.abs(np.dot(srand, srand.T))
+  diag = np.reshape(osim[lsim==1],-1)
+  off_diag = np.reshape(osim[lsim==0],-1)
+  baseline_diag = np.reshape(rsim[lsim==1],-1)
+  baseline_off_diag = np.reshape(rsim[lsim==0],-1)
+  return (np.mean(diag), np.std(diag), \
+          np.mean(off_diag), np.std(off_diag), \
+          np.mean(baseline_diag), np.std(baseline_diag), \
+          np.mean(baseline_off_diag), np.std(baseline_off_diag))
 
-  return stats
-  
 
 if __name__ == "__main__":
   debug_opts = options.get_opts()
@@ -146,16 +111,16 @@ if __name__ == "__main__":
     stats = np.zeros((n,8))
     if opts.verbose:
       for i in range(n):
-        stats[i] = experiment(opts, network, i)
+        stats[i] = get_stats(opts, network, i)
     else:
       for i in tqdm.tqdm(range(n)):
-        stats[i] = experiment(opts, network, i)
+        stats[i] = get_stats(opts, network, i)
     meanstats = np.mean(stats,0)
     print("Diag: {:.2e} +/- {:.2e}, Off Diag: {:.2e} +/- {:.2e}, " \
           "Baseline Diag: {:.2e} +/- {:.2e}, " \
           "Baseline Off Diag: {:.2e} +/- {:.2e}".format(*list(meanstats)))
   elif opts.debug_plot == 'plot':
-    experiment(opts, network, opts.debug_index)
+    plot_index(opts, network, opts.debug_index)
   elif opts.debug_plot == 'baseline':
     plot_baseline(opts, network, opts.debug_index)
 
