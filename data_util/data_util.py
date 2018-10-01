da299f77e64748a6126422a5614ee18e8e8086f7
Stephen Phillips <chengdu.scp@gmail.com>
2018-10-01 10:38:52 -0400

    Added new module for data (will clean up soon)

--- data_util/data_util.py
+++ data_util/data_util.py
@@ -12,60 +12,7 @@ import tensorflow as tf
 import options
 import myutils
 import sim_graphs
-
-# Tensorflow features
-def _bytes_feature(value):
-  """Create arbitrary tensor Tensorflow feature."""
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-def _int64_feature(value):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-class Int64Feature(object):
-  """Custom class used for decoding serialized tensors."""
-  def __init__(self, key, description):
-    super(Int64Feature, self).__init__()
-    self._key = key
-    self.description = description
-    self.shape = []
-    self.dtype = 'int64'
-
-  def get_placeholder(self):
-    return tf.placeholder(tf.int64, shape=[None])
-
-  def get_feature_write(self, value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-  def get_feature_read(self):
-    return tf.FixedLenFeature([], tf.int64)
-
-  def tensors_to_item(self, keys_to_tensors):
-    tensor = keys_to_tensors[self._key]
-    return tf.cast(tensor, dtype=tf.int64)
-
-class TensorFeature(object):
-  """Custom class used for decoding serialized tensors."""
-  def __init__(self, key, shape, dtype, description):
-    super(TensorFeature, self).__init__()
-    self._key = key
-    self.shape = shape
-    self.dtype = dtype
-    self.description = description
-
-  def get_placeholder(self):
-    return tf.placeholder(self.dtype, shape=[None] + self.shape)
-
-  def get_feature_write(self, value):
-    v = value.astype(self.dtype).tobytes()
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[v]))
-
-  def get_feature_read(self):
-    return tf.FixedLenFeature([], tf.string)
-
-  def tensors_to_item(self, keys_to_tensors):
-    tensor = keys_to_tensors[self._key]
-    tensor = tf.decode_raw(tensor, out_type=self.dtype)
-    return tf.reshape(tensor, self.shape)
+from data_util import tf_helpers
 
 class GraphSimDataset(object):
   """Dataset for Cycle Consistency graphs"""
@@ -88,45 +35,54 @@ class GraphSimDataset(object):
     f = opts.final_embedding_dim
     self.features = {
       'InitEmbeddings':
-           TensorFeature(key='InitEmbeddings',
+           tf_helpers.TensorFeature(
+                         key='InitEmbeddings',
                          shape=[d, e],
                          dtype=self.dtype,
                          description='Initial embeddings for optimization'),
       'AdjMat':
-           TensorFeature(key='AdjMat',
+           tf_helpers.TensorFeature(
+                         key='AdjMat',
                          shape=[d, d],
                          dtype=self.dtype,
                          description='Adjacency matrix for graph'),
       'Degrees':
-           TensorFeature(key='Degrees',
+           tf_helpers.TensorFeature(
+                         key='Degrees',
                          shape=[d, d],
                          dtype=self.dtype,
                          description='Degree matrix for graph'),
       'Laplacian':
-           TensorFeature(key='Laplacian',
+           tf_helpers.TensorFeature(
+                         key='Laplacian',
                          shape=[d, d],
                          dtype=self.dtype,
                          description='Alternate Laplacian matrix for graph'),
       'Mask':
-           TensorFeature(key='Mask',
+           tf_helpers.TensorFeature(
+                         key='Mask',
                          shape=[d, d],
                          dtype=self.dtype,
                          description='Mask for valid values of matrix'),
       'MaskOffset':
-           TensorFeature(key='MaskOffset',
+           tf_helpers.TensorFeature(
+                         key='MaskOffset',
                          shape=[d, d],
                          dtype=self.dtype,
                          description='Mask offset for loss'),
       'TrueEmbedding':
-           TensorFeature(key='TrueEmbedding',
+           tf_helpers.TensorFeature(
+                         key='TrueEmbedding',
                          shape=[d, p],
                          dtype=self.dtype,
                          description='True values for the low dimensional embedding'),
       'NumViews':
-           Int64Feature(key='NumViews',
+           tf_helpers.Int64Feature(
+                         key='NumViews',
                          description='Number of views used in this example'),
       'NumPoints':
-           Int64Feature(key='NumPoints',
+           tf_helpers.Int64Feature(
+                         key='NumPoints',
                          description='Number of points used in this example'),
     }
 
