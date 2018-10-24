f2496b3df4467f7a1986ec39a616471828d1e13b
Stephen Phillips <chengdu.scp@gmail.com>
2018-10-24 15:25:25 -0400

    Added some stuff for better plotting of the downloaded files

--- launch/download_files.py
+++ launch/download_files.py
@@ -49,23 +49,29 @@ def get_logs(log_names, yaml_name):
   return [ lb.decode("utf-8").split("\n") for lb in logs_bytes ]
 
 def get_loss_values(log):
-  loss = []
-  test_loss = []
-  gt_loss = []
+  loss_vals = {
+    'train': [],
+    'test': [],
+    'gt_l1': [],
+    'gt_l2': [],
+  }
   for x in log:
     line = x.split()
-    if '=' in line:
+    if '=' in line: # Marks where the 
       if 'Test' in line:
-        test_loss_val = line[line.index('=')+1]
-        if test_loss_val[-1] not in '0123456789.':
-          test_loss_val = test_loss_val[:-1]
-        test_loss.append(float(test_loss_val))
-        if 'GT' in line:
-          gt_loss_val = line[line.index('GT') + 2]
-          gt_loss.append(float(gt_loss_val))
+        loss_vals['test'].append(float(line[line.index('=')+1]))
+        if 'L1' in line:
+          loss_vals['gt_l1'].append(float(line[line.index('L1') + 3]))
+        if 'L2' in line:
+          loss_vals['gt_l2'].append(float(line[line.index('L2') + 3]))
       else:
-        loss.append(float(line[line.index('=')+1]))
-  return loss, test_loss, gt_loss
+        loss_vals['train'].append(float(line[line.index('=')+1]))
+  for k in list(loss_vals.keys()):
+    if len(loss_vals[k]) == 0:
+      del loss_vals[k]
+    else:
+      loss_vals[k] = np.array(loss_vals[k])
+  return loss_vals
 
 opts = get_opts()
 print("Getting data...")
@@ -75,21 +81,21 @@ for n in range(len(opts.experiments)):
   yaml_name = opts.experiments[n]
   name = os.path.splitext(yaml_name)[0]
   logs = get_logs(log_names, name)
-  if len(logs) > 1:
+  if len(logs) > 0:
     for l in range(len(logs)):
-      train_loss, test_loss, gt_loss = np.array(get_loss_values(logs[l]))
-      loss_name ="{}-{}".format(name,l)
-      losses.append((train_loss, test_loss, gt_loss, loss_name, name))
+      loss_vals = get_loss_values(logs[l])
+      if len(logs) > 1:
+        loss_name ="{}-{}".format(name,l)
+      else:
+        loss_name = name
+      losses.append((loss_vals, loss_name, name))
   elif len(logs) == 0:
     print("ERROR - {} has no logs".format(yaml_name))
     sys.exit(1)
-  else:
-    loss, test_loss, gt_loss = np.array(get_loss_values(logs[0]))
-    losses.append((loss, test_loss, gt_loss, name, name))
 
-print([ (name, len(l), len(tl), len(gtl)) for l, tl, gtl, name, _ in losses ])
+print([ (name, ( "{}: {}".format(k,len(v)) for k, v in l.items() )) for l, name, _ in losses ])
 path="/home/stephen/cycleconsistencynet/save/save"
-for loss, test_loss, gt_loss, loss_name, path_name in losses:
+for loss_vals, loss_name, path_name in losses:
   print("Loss {}".format(loss_name))
   dirpath = "../logs/save-{}".format(loss_name)
   if not os.path.exists(dirpath):
@@ -99,26 +105,24 @@ for loss, test_loss, gt_loss, loss_name, path_name in losses:
       "stephen@kostas-ap.grasp.upenn.edu:{}-{}".format(path,path_name),
       dirpath
     ])
-    np.save(os.path.join(dirpath, 'loss.npy'), loss)
-    np.save(os.path.join(dirpath, 'test_loss.npy'), test_loss)
-    if gt_loss:
-      np.save(os.path.join(dirpath, 'gt_loss.npy'), gt_loss)
+    np.save(os.path.join(dirpath, 'loss.npy'), loss_vals['train'])
+    np.save(os.path.join(dirpath, 'test_loss.npy'), loss_vals['test'])
+    for k in (set(loss_vals.keys()) - {'train','test'}):
+      np.save(os.path.join(dirpath, '{}_loss.npy'.format(k)), loss_vals[k])
 
-nplots = 2
-if any([ loss_vals[2] != [] for loss_vals in losses ]):
-  nplots = 3
-fig, ax = plt.subplots(nrows=1, ncols=nplots)
-for i in range(len(losses)):
-  ax[0].plot(losses[i][0], label=losses[i][3])
-  ax[1].plot(losses[i][1], label=losses[i][3])
-  if nplots == 3:
-    ax[2].plot(losses[i][2], label=losses[i][3])
-ax[0].legend()
-ax[1].legend()
-if len(ax) > 2:
-  ax[2].legend()
+print(losses)
+loss_keys = sorted(list(set().union(*( set(l.keys()) for l, _, _ in losses ))), reverse=True)
+fig, ax = plt.subplots(nrows=1, ncols=len(loss_keys))
+for a, k in enumerate(loss_keys):
+  for i in range(len(losses)):
+    if k in losses[i][0]:
+      ax[a].plot(losses[i][0][k], label=losses[i][1])
+  ax[a].set_title('{} Loss'.format(k.title()))
+  ax[a].legend()
+
 plt.show()
 print("\n".join([
-        "{}, {}: {} ({})".format(n, l[-1], tl[-1], len(l)) for l, tl, _, n, _ in losses
+        "{}, {}: {} ({})".format(n, l['train'][-1], l['test'][-1], len(l['train']))
+        for l, n, _ in losses
       ]))
 
