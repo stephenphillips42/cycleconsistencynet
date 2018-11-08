ff00b08f864977c7e329f909bfd8f647640a5551
Stephen Phillips <chengdu.scp@gmail.com>
2018-11-08 14:18:14 -0500

    Fixed download files to use the new log file instead of the klogs

--- launch/download_files.py
+++ launch/download_files.py
@@ -23,30 +23,6 @@ def get_opts():
   opts = parser.parse_args()
   return opts
 
-def in_set(x, S):
-  for s in S:
-    if x in s:
-      return True
-  return False
-
-def get_log_names(opts):
-  experiments = []
-  for name in opts.experiments:
-    yaml_name = os.path.splitext(name)[0]
-    if len(yaml_name) > 56:
-      yaml_name = yaml_name[:56]
-    experiments.append(yaml_name) 
-  raw = subprocess.check_output(["kubectl", "get", "pods"])
-  decoded = [ z.split() for z in raw.decode("utf-8").split("\n") ]
-  log_names_all = [ w[0] for w in decoded[:-1] ]
-  exclude = [ 'kssh-session', 'NAME' ] + opts.exclude
-  log_names = [ w for w in log_names_all if not in_set(w, exclude) ]
-  return log_names
-
-def get_logs(log_names, yaml_name):
-  sel_names = [ w for w in log_names if yaml_name in w  ]
-  logs_bytes = [ subprocess.check_output(["kubectl", "logs", log_name]) for log_name in sel_names ]
-  return [ lb.decode("utf-8").split("\n") for lb in logs_bytes ]
 
 def get_loss_values(log):
   loss_vals = {
@@ -57,7 +33,7 @@ def get_loss_values(log):
   }
   for x in log:
     line = x.split()
-    if '=' in line: # Marks where the 
+    if '=' in line: # Marks where the loss value is
       if 'Test' in line:
         loss_vals['test'].append(float(line[line.index('=')+1]))
         if 'L1' in line:
@@ -74,44 +50,37 @@ def get_loss_values(log):
   return loss_vals
 
 opts = get_opts()
+assert(all([ x[-len('.yaml'):] == '.yaml' for x in opts.experiments ]))
 print("Getting data...")
-losses = []
-log_names = get_log_names(opts)
-for n in range(len(opts.experiments)):
-  yaml_name = opts.experiments[n]
-  name = os.path.splitext(yaml_name)[0]
-  logs = get_logs(log_names, name)
-  if len(logs) > 0:
-    for l in range(len(logs)):
-      loss_vals = get_loss_values(logs[l])
-      if len(logs) > 1:
-        loss_name ="{}-{}".format(name,l)
-      else:
-        loss_name = name
-      losses.append((loss_vals, loss_name, name))
-  elif len(logs) == 0:
-    print("ERROR - {} has no logs".format(yaml_name))
-    sys.exit(1)
+names = [ x[:-len('.yaml')] for x in opts.experiments ]
 
-print([ (name, ( "{}: {}".format(k,len(v)) for k, v in l.items() )) for l, name, _ in losses ])
-path="/home/stephen/cycleconsistencynet/save/save"
-for loss_vals, loss_name, path_name in losses:
-  print("Loss {}".format(loss_name))
-  dirpath = "../logs/save-{}".format(loss_name)
-  if not os.path.exists(dirpath):
+save_path="/home/stephen/cycleconsistencynet/save/save"
+losses = []
+for name in names:
+  print("Loss {}".format(name))
+  logs_path = os.path.join("..","logs","save-{}".format(name))
+  if not os.path.exists(logs_path):
+    print([
+      "scp",
+      "-r",
+      "stephen@kostas-ap.grasp.upenn.edu:{}-{}".format(save_path,name),
+      logs_path
+    ])
     subprocess.call([
       "scp",
       "-r",
-      "stephen@kostas-ap.grasp.upenn.edu:{}-{}".format(path,path_name),
-      dirpath
+      "stephen@kostas-ap.grasp.upenn.edu:{}-{}".format(save_path,name),
+      logs_path
     ])
-    np.save(os.path.join(dirpath, 'loss.npy'), loss_vals['train'])
-    np.save(os.path.join(dirpath, 'test_loss.npy'), loss_vals['test'])
+    with open(os.path.join(logs_path, 'logfile.log'), 'r') as log:
+      loss_vals = get_loss_values(log)
+    losses.append((loss_vals, name))
+    np.save(os.path.join(logs_path, 'loss.npy'), loss_vals['train'])
+    np.save(os.path.join(logs_path, 'test_loss.npy'), loss_vals['test'])
     for k in (set(loss_vals.keys()) - {'train','test'}):
-      np.save(os.path.join(dirpath, '{}_loss.npy'.format(k)), loss_vals[k])
+      np.save(os.path.join(logs_path, '{}_loss.npy'.format(k)), loss_vals[k])
 
-print(losses)
-loss_keys = sorted(list(set().union(*( set(l.keys()) for l, _, _ in losses ))), reverse=True)
+loss_keys = sorted(list(set().union(*( set(l.keys()) for l, _ in losses ))), reverse=True)
 fig, ax = plt.subplots(nrows=1, ncols=len(loss_keys))
 for a, k in enumerate(loss_keys):
   for i in range(len(losses)):
@@ -121,8 +90,4 @@ for a, k in enumerate(loss_keys):
   ax[a].legend()
 
 plt.show()
-print("\n".join([
-        "{}, {}: {} ({})".format(n, l['train'][-1], l['test'][-1], len(l['train']))
-        for l, n, _ in losses
-      ]))
 
